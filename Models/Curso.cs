e132ec9102435802b30cad81d22908077decef4e
nicolasolivet <nicolasolivet93@gmail.com>
2025-12-04 00:45:10 -0300

    primeras clases

---
+++ Models/Curso.cs
@@ -0,0 +1,14 @@
+﻿namespace ctrlcctrlv.Models
+{
+    public class Curso
+    {
+        public int CursoId { get; set; }
+        public int Anio { get; set; }
+        public int Division { get; set; }
+        public string Turno { get; set; }
+
+        public ICollection<Alumno> Alumnos { get; set; }
+
+
+    }
+}
